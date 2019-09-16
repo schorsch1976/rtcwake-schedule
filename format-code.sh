32361755a9ed125b79cb5dac61a8a3f999ee6e86
Georg Gast <georg@schorsch-tech.de>
2019-09-16 10:50:55 +0200

    clang-format

--- format-code.sh
+++ format-code.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find src/ -iname "*.cpp" -exec clang-format-3.9 -i {} \;
-find src/ -iname "*.h" -exec clang-format-3.9 -i {} \;
+find src/ -iname "*.cpp" -exec clang-format -i {} \;
+find src/ -iname "*.h" -exec clang-format -i {} \;
